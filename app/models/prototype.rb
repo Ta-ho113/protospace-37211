b8cb6ffbde5813ac6f4d15472c2f0aafa1dd8314
Ta-ho113 <takuma.04.soka@gmail.com>
2022-03-17 19:47:17 +0900

    バリデーションの追加記入

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,5 +4,6 @@ class Prototype < ApplicationRecord
   validates :title,        presence: true
   validates :catch_copy,   presence: true
   validates :occupation,   presence: true
+  validates :image,        presence: true
   has_many  :comments
 end
